1cc21f06926647792909ea0d529649275f7c5c64
Mo'men Gharib <momengharib22@hotmail.com>
2022-12-28 21:18:49 +0200

    Revert "SignUP page"
    This reverts commit b1da823d

---
+++ src/main/java/db/bookstore/controllers/SignUpController.java
@@ -0,0 +1,39 @@
+package db.bookstore.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SignUpController {
+
+    @FXML
+    private TextField Address;
+
+    @FXML
+    private TextField Fname;
+
+    @FXML
+    private TextField Lname;
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private TextField number;
+
+    @FXML
+    private TextField pass;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private TextField userName;
+
+    @FXML
+    void OnSignUP(ActionEvent event) {
+
+    }
+
+}
