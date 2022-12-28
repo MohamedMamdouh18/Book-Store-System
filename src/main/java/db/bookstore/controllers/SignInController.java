42aa8d3472d6d4df08520cf0fafa34f92b148c91
ziadreda72 <engziadreda@gmail.com>
2022-12-28 20:56:14 +0200

    SignIn page

---
+++ src/main/java/db/bookstore/controllers/SignInController.java
@@ -0,0 +1,24 @@
+package db.bookstore.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class SignInController {
+
+    @FXML
+    private TextField PasswordTextField;
+
+    @FXML
+    private Button SignInButton;
+
+    @FXML
+    private TextField userNameTextField;
+
+    @FXML
+    void signInOnAction(ActionEvent event) {
+
+    }
+
+}
