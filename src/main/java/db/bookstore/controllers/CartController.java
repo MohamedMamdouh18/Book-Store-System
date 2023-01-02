90e1cb01d97340ffb78951f04b32731ae3e971a3
ziadreda72 <engziadreda@gmail.com>
2023-01-02 23:49:21 +0200

    working on CartController

--- src/main/java/db/bookstore/controllers/CartController.java
+++ src/main/java/db/bookstore/controllers/CartController.java
@@ -1,23 +1,36 @@
 package db.bookstore.controllers;
 
+import Database.DAO.CustomerDAO;
+import Database.Models.Book;
+import db.bookstore.UserInfo;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
+import lombok.SneakyThrows;
 
-public class CartController {
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.*;
+
+public class CartController implements Initializable {
 
     @FXML
-    private TableView<?> bookTable;
+    private TableView<Book> bookTable;
 
     @FXML
     private Button cancelButton;
 
     @FXML
-    private TableColumn<?, ?> category;
+    private TableColumn<Book, String> category;
 
     @FXML
     private TextField creditCardHolderTextField;
@@ -38,22 +51,45 @@ public class CartController {
     private TextField idTextField;
 
     @FXML
-    private TableColumn<?, ?> isbn;
+    private TableColumn<Book, String> isbn;
 
     @FXML
-    private TableColumn<?, ?> price;
+    private TableColumn<Book, Float> price;
 
     @FXML
-    private TableColumn<?, ?> publicationYear;
+    private TableColumn<Book, Date> publicationYear;
 
     @FXML
-    private TableColumn<?, ?> publisher;
+    private TableColumn<Book, String> publisher;
 
     @FXML
-    private TableColumn<?, ?> quantity;
+    private TableColumn<Book, Integer> quantity;
 
     @FXML
-    private TableColumn<?, ?> title;
+    private TableColumn<Book, String> title;
+
+    @SneakyThrows
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        isbn.setCellValueFactory(new PropertyValueFactory<Book, String>("isbn"));
+        title.setCellValueFactory(new PropertyValueFactory<Book, String>("title"));
+        publicationYear.setCellValueFactory(new PropertyValueFactory<Book, Date>("publication_year"));
+        price.setCellValueFactory(new PropertyValueFactory<Book, Float>("price"));
+        category.setCellValueFactory(new PropertyValueFactory<Book, String>("category"));
+        quantity.setCellValueFactory(new PropertyValueFactory<Book, Integer>("stock"));
+        publisher.setCellValueFactory(new PropertyValueFactory<Book, String>("publisher_name"));
+
+        Map < Book , Integer > map = new HashMap<>();
+        for (Book book : UserInfo.userCart){
+            if (!map.containsKey(book)){
+                map.put(book , 1);
+            }else{
+                map.put(book , map.get(book) + 1);
+            }
+        }
+        List < Book > books = new ArrayList<>();
+
+    }
 
     @FXML
     void cancelButtonOnAction(ActionEvent event) {
