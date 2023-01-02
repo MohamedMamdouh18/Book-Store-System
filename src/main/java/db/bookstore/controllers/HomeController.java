27f57745e5f7b95433941c1c788beb57e4ec3493
Mohamed Mamdouh <mohamadmmdouh10@gmail.com>
2023-01-02 20:19:21 +0200

    some work

---
+++ src/main/java/db/bookstore/controllers/HomeController.java
@@ -0,0 +1,19 @@
+package db.bookstore.controllers;
+
+import javafx.fxml.Initializable;
+import lombok.SneakyThrows;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
+//
+//    @FXML
+//    private TableView<> bookTable;
+
+    @SneakyThrows
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
+
+}
