2babf361b2d92f34db3978eacbd657ca9e8efee9
Mohamed Mamdouh <mohamadmmdouh10@gmail.com>
2023-01-05 00:27:06 +0200

    User Profile Page is Finished

--- src/main/java/db/bookstore/controllers/UserProfileController.java
+++ src/main/java/db/bookstore/controllers/UserProfileController.java
@@ -1,24 +1,24 @@
 package db.bookstore.controllers;
 
+import Database.DAO.CustomerDAO;
+import db.bookstore.UserInfo;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class UserProfileController {
+import java.net.URL;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class UserProfileController implements Initializable {
 
     @FXML
     private TextField emailAddress;
 
     @FXML
     private TextField firstName;
-
-    @FXML
-    private Label firstNameWelcomeLabel;
-
-    @FXML
-    private Button homeReturningButton;
 
     @FXML
     private TextField lastName;
@@ -30,22 +30,44 @@ public class UserProfileController {
     private TextField phoneNumber;
 
     @FXML
-    private Button saveChangesButton;
-
-    @FXML
     private TextField shipping;
 
     @FXML
-    private Button signOutButton;
+    private Label savedChanges;
 
     @FXML
-    void homeReturningOnAction(ActionEvent event) {
+    private Label error;
 
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        emailAddress.setText(UserInfo.currentUser.getEmail_address());
+        firstName.setText(UserInfo.currentUser.getFirst_name());
+        lastName.setText(UserInfo.currentUser.getLast_name());
+        password.setText(UserInfo.currentUser.getPassword());
+        phoneNumber.setText(UserInfo.currentUser.getPhone_number());
+        shipping.setText(UserInfo.currentUser.getAddress());
+        savedChanges.setVisible(false);
+        error.setVisible(false);
     }
 
     @FXML
-    void saveChangesOnAction(ActionEvent event) {
+    void saveChangesOnAction(ActionEvent event) throws SQLException {
+        try {
+            var DAO = CustomerDAO.getInstance();
+            var user = UserInfo.currentUser;
+            DAO.changeUserProfile(user.getUsername(), "email_address", emailAddress.getText());
+            DAO.changeUserProfile(user.getUsername(), "first_name", firstName.getText());
+            DAO.changeUserProfile(user.getUsername(), "last_name", lastName.getText());
+            DAO.changeUserProfile(user.getUsername(), "password", password.getText());
+            DAO.changeUserProfile(user.getUsername(), "phone_number", phoneNumber.getText());
+            DAO.changeUserProfile(user.getUsername(), "address", shipping.getText());
+            savedChanges.setVisible(true);
+            error.setVisible(false);
+        } catch (Exception e) {
+            error.setVisible(true);
+            savedChanges.setVisible(false);
+        }
 
     }
-
 }
